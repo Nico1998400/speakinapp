2cf770313ae3c5bb15475bdedc6147a0e5d128bb
Nicolai Stahl <nico1998@live.se>
2023-02-02 16:17:14 +0100

    Fixed some more on AudioRecord, Added So we save the recording in reacrodings

--- components/AudioRecord.js
+++ components/AudioRecord.js
@@ -1,9 +1,10 @@
 import * as React from 'react';
-import { View, Button, StyleSheet } from 'react-native';
+import { View, Button, StyleSheet} from 'react-native';
 import { Audio } from 'expo-av';
 
 export default function AudioRecorder() {
   const [recording, setRecording] = React.useState();
+  const [recordings, setRecordings] = React.useState([]);
   const [sound, setSound] = React.useState();
 
 
@@ -17,7 +18,7 @@ export default function AudioRecorder() {
       });
 
       console.log('Starting recording..');
-      const { recording } = await Audio.Recording.createAsync( Audio.RecordingOptionsPresets.HIGH_QUALITY
+      const { recording } = await Audio.Recording.createAsync(Audio.RecordingOptionsPresets.HIGH_QUALITY
       );
       setRecording(recording);
       console.log('Recording started');
@@ -27,24 +28,36 @@ export default function AudioRecorder() {
   }
 
   async function stopRecording() {
-    console.log('Stopping recording..');
     setRecording(undefined);
     await recording.stopAndUnloadAsync();
-    await Audio.setAudioModeAsync({
-      allowsRecordingIOS: false,
+
+    let updatedRecordings = [...recordings];
+    const { sound, status } = await recording.createNewLoadedSoundAsync();
+    updatedRecordings.push({
+      sound: sound,
+      duration: getDurationFormatted(status.durationMillis),
+      file: recording.getURI()
     });
-    const uri = recording.getURI();
-    console.log('Recording stopped and stored at', uri);
-    await sound.playAsync();
+    console.log(updatedRecordings);
+    setRecordings(updatedRecordings);
+  }
+
+  function getDurationFormatted(millis) {
+    const minutes = millis / 1000 / 60;
+    const minutesDisplay = Math.floor(minutes);
+    const seconds = Math.round((minutes - minutesDisplay) * 60);
+    const secondsDisplay = seconds < 10 ? `0${seconds}` : seconds;
+    return `${minutesDisplay}:${secondsDisplay}`;
   }
 
   async function playSound() {
     console.log('Loading Sound');
     const sound = new Audio.Sound()
-    await Audio.Sound.loadAsync({
-      uri: {uri}
-  })
+    await sound.loadAsync({
+      
+    })
     setSound(sound);
+    console.log(sound)
     console.log('Playing Sound');
     await sound.playAsync();
   }
@@ -52,12 +65,11 @@ export default function AudioRecorder() {
   React.useEffect(() => {
     return sound
       ? () => {
-          console.log('Unloading Sound');
-          sound.unloadAsync();
-        }
+        console.log('Unloading Sound');
+        sound.unloadAsync();
+      }
       : undefined;
   }, [sound]);
-
 
   return (
     <View style={styles.container}>
@@ -65,16 +77,16 @@ export default function AudioRecorder() {
         title={recording ? 'Stop Recording' : 'Start Recording'}
         onPress={recording ? stopRecording : startRecording}
       />
-       <Button title="Play Sound" onPress={playSound} />
+      <Button title="Play Sound" onPress={playSound} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  
-  container:{
-  paddingTop: 300,
-  fontSize: 24,
-  color:"black",
+
+  container: {
+    paddingTop: 300,
+    fontSize: 24,
+    color: "black",
   }
 });
