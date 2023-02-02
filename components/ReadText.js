5e2332fe60944e51db52f666bcb9cbdb6090de18
Tim Andersson <Tim.1996@live.se>
2023-02-02 12:52:25 +0100

    Added language with Json

--- components/ReadText.js
+++ components/ReadText.js
@@ -8,9 +8,12 @@ export default function ReadText(){
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
   console.log(data);
-
+// https://mocki.io/v1/3158625d-eeb4-45f9-ab43-9e5751b99006   Spanish
+// https://mocki.io/v1/698e9048-826e-43b1-b00a-2ccf4aa03baf   Sve
+// https://mocki.io/v1/380722bc-b8a6-441b-bbf8-d0ad1f343b70 Eng
+// https://mocki.io/v1/34b91116-1dbf-4456-9f7e-b41ae8aeecbf thai
   useEffect(() => {
-    fetch('https://mocki.io/v1/114bcd25-c115-4aac-a937-473cdffaacf3')
+    fetch('https://mocki.io/v1/34b91116-1dbf-4456-9f7e-b41ae8aeecbf')
       .then((response) => response.json())
       .then((json) => setData(json))
       .catch((error) => console.error(error))
@@ -19,17 +22,17 @@ export default function ReadText(){
 
   const speak = () => {
     data.map((item, index)=>
-     Speech.speak(item.question)
-  
-    
+     Speech.speak(item.question, {
+      language: item.language
+     }
+     )
     )
   }
 
   return (
     <View>
 
-
-    <View style={{ flex: 1, padding: 24 }}>
+     <View style={{ flex: 1, padding: 24 }}>
 
     {isLoading ? <Text>Loading...</Text> : 
     ( <View>
@@ -42,13 +45,10 @@ export default function ReadText(){
       </View>
     )}
 
-  </View>
+   </View>
   </View>
   );
-
-
 };
-
 
 const stylestext = StyleSheet.create({
             baseText: {
